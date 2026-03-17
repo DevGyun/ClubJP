4d463636d80c743ae53d9f5f9b8c542b123d0fed
DevGyun <denuvyg11436@gmail.com>
2026-03-17 21:45:39 +0900

    Update py.py

--- py.py
+++ py.py
@@ -1,15 +1,1 @@
-<<<<<<< HEAD
-a='강아지'
-print(a)
-print("new")
-print("new")
-print("new")
-print("new")
-print("new")
-print("new")
-print("new")
-print("new")
-print("second part")
-=======
-#pull request
->>>>>>> 9710cea775bf4a6e9a937c81241ef9cba0b6afff
+#reset
